5b4f421c54578896bfc4aa3c2c8a8ac8bc93643b
srijan <sibu.stephen@srijan.in>
2016-01-20 18:28:18 +0530

    Current position and markers placed

--- map-controller.js
+++ map-controller.js
@@ -1,10 +1,15 @@
 angular.module('maps-render',[])
-.controller("MapRenderingController", function ($scope) {
+.controller("MapRenderingController", function ($scope,$http) {
+   var bounds = new google.maps.LatLngBounds();
+   navigator.geolocation.getCurrentPosition(function(position) {
+        
+  var geolocate = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
   var myOptions = {
-    center: {lat: -33.8688, lng: 151.2195},
-    zoom: 10,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
+    center: geolocate,
+    zoom: 20
   }
+
+  var infowindow = new google.maps.InfoWindow();
   var mapy = new google.maps.Map(document.getElementById('map'), myOptions);
   // Create the search box and link it to the UI element.
   var input = document.getElementById('searchbar');
@@ -20,43 +25,32 @@ angular.module('maps-render',[])
   
   searchBox.addListener('places_changed', function() {
     var places = searchBox.getPlaces();
- 
-    markers.forEach(function(marker) {
-      marker.setMap(null);
-    });
-    markers = [];
-    // For each place, get the icon, name and location.
-    var bounds = new google.maps.LatLngBounds();
+
+  /*Marker Placement from json data*/
+   $http.get("goa-bikes.json").then(function(response) {
+        var er = response.data.Bikes;
    
-    places.forEach(function(place) {
-      var icon = {
-        url: place.icon,
-        size: new google.maps.Size(71, 71),
-        origin: new google.maps.Point(0, 0),
-        anchor: new google.maps.Point(17, 34),
-        scaledSize: new google.maps.Size(20, 20)
-      };
-      // Create a marker for each place.
-      var infowindow = new google.maps.InfoWindow();
-      markers.push(new google.maps.Marker({
-        map: mapy,
-        icon: icon,
-        title: place.name,
-        position: place.geometry.location
-      })); markers.forEach(function(markerq) {
+
+    for (var i = 0; i < er.length; i++) {
+            var data = er[i];
+            var myLatlng = new google.maps.LatLng(data.lat, data.long);
+            var marker = new google.maps.Marker({
+                position: myLatlng,
+                map: mapy,
+                title: data.title
+            });
+            (function (markerq, data) {
                 google.maps.event.addListener(markerq, "click", function (e) {
-                    // infowindow.setContent("<div style = 'width:200px;min-height:40px'>" +markerq.title + "</div>");
-                    // infowindow.open(map, markerq);
-                    alert("Place name - "+ markerq.title);
+                  // alert("Place name - "+ markerq.title);
+                    infowindow.setContent("<div style = 'width:200px;min-height:40px'>" + data.Address + "</div>");
+                    infowindow.open(map, markerq);
                 });
-            })
-      if (place.geometry.viewport) {
-        // Only geocodes have viewport.
-        bounds.union(place.geometry.viewport);
-      } else {
-        bounds.extend(place.geometry.location);
-      }
-    });
+            })(marker, data);
+            bounds.extend(marker.position);
+        }
+        mapy.setCenter(bounds.getCenter());
+});
+   });
     $scope.click_me = function() {
       mapy.fitBounds(bounds);
     }
